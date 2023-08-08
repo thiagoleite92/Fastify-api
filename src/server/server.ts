9e2592091b2885793788c36909627ac59fc0df54
Thiago Leite <leite.tjs@gmail.com>
2023-08-07 23:06:08 -0300

    feat: api route check

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,10 @@
 import { app } from '../app/app';
 import { env } from '../env';
 
+app.get('/health', async (req, replay) => {
+  return replay.status(200).send('API ONLINE');
+});
+
 app.listen({ port: env.PORT }, (err, address) => {
   console.log(`Server is now listening on ${address}`);
 });

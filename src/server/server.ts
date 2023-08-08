30ddd89599a24bb2656894e3c91c386351905fad
Thiago Leite <leite.tjs@gmail.com>
2023-08-08 01:37:49 -0300

    try fix

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,6 +5,6 @@ app.get('/health', async (req, replay) => {
   return replay.status(200).send('API ONLINE');
 });
 
-app.listen({ port: env.PORT }, (err, address) => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }, (err, address) => {
   console.log(`Server is now listening on ${address}`);
 });

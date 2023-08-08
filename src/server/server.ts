201c02bede5102182ebffc99f14f0d2972744e28
Thiago Leite <leite.tjs@gmail.com>
2023-08-08 02:02:08 -0300

    ultimo fix

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,11 @@
 import { app } from '../app/app';
 import { env } from '../env';
 
-app.get('/health', async (req, replay) => {
-  return replay.status(200).send('API ONLINE');
-});
-
-app.listen({ port: env.PORT, host: '0.0.0.0' }, (err, address) => {
-  console.log(`Server is now listening on ${address}`);
-});
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!');
+  });
